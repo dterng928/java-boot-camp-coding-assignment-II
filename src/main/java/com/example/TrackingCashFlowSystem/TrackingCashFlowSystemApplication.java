7e40a2b4229938d7d68dc03a68328c74175a72de
dterng928 <bhatarasiri_cud@hotmail.com>
2022-03-20 10:37:31 +0700

    chore : setup project

---
+++ src/main/java/com/example/TrackingCashFlowSystem/TrackingCashFlowSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrackingCashFlowSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingCashFlowSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingCashFlowSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TrackingCashFlowSystem/TrackingCashFlowSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrackingCashFlowSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingCashFlowSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
